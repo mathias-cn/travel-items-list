537a565d5289e3ef9d22ae0d400516417d51a033
Mathias Navarre <mathiascnavarre@gmail.com>
2024-07-18 20:01:27 -0300

    Changed little CSS

--- src/components/clear-list-modal.tsx
+++ src/components/clear-list-modal.tsx
@@ -5,7 +5,7 @@ interface ClearListModalProps {
 
 export function ClearListModal({ closeConfirmClearModal, clearList }: ClearListModalProps) {
     return (
-        <div className="fixed w-full h-screen bg-[rgba(0,0,0,.5)] flex items-center justify-center gap-4 text-center">
+        <div className="fixed w-full h-screen bg-[rgba(0,0,0,.5)] flex items-center justify-center gap-4 text-center z-50">
           <div className="bg-orange-950 rounded-xl p-12 font-semibold space-y-4">
             <p className="text-xl text-amber-200">WARNING!</p>
             <p className="text-xl text-amber-200">You're going to delete<br />all items from the list!</p>
